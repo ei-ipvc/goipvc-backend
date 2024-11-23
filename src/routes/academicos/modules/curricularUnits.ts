eda67a3b4ebe6c7b70e55850ed751cc942598f3e
Matthew Rodrigues <matt@capaz.dev>
2024-11-23 17:50:40 +0000

    feat: format curricular units response

--- src/routes/academicos/modules/curricularUnits.ts
+++ src/routes/academicos/modules/curricularUnits.ts
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import axios from "axios";
 
+interface FormattedUnit {
+  id: number;
+  name: string;
+  finalGrade: string | number;
+  academicYear: string;
+  studyYear: number;
+  semester: number;
+  ects: number;
+}
+
 const router = Router();
 
 router.post("/", async (req, res) => {
@@ -11,9 +21,10 @@ router.post("/", async (req, res) => {
     return;
   }
 
+  // 'https://academicos.ipvc.pt/netpa/ajax/consultanotasaluno/inscricoes?cdLectivoFilter=null&periodoFilter=null&anoCurricular=null&estadoFilter=null&disciplinaFilter=null',
   try {
     const response = await axios.get(
-      'https://academicos.ipvc.pt/netpa/ajax/consultanotasaluno/inscricoes?cdLectivoFilter=null&periodoFilter=null&anoCurricular=null&estadoFilter=null&disciplinaFilter=null&group=[{"property":"CD_LECTIVO","direction":"desc"}]&sort=[{"property":"CD_LECTIVO","direction":"DESC"}]',
+      "https://academicos.ipvc.pt/netpa/ajax/consultanotasaluno/inscricoes",
       {
         headers: {
           Cookie: token,
@@ -21,13 +32,29 @@ router.post("/", async (req, res) => {
       }
     );
 
-    res.status(response.status).json(response.data);
+    const formattedUnits: FormattedUnit[] = response.data.result.map(
+      (item: any) => ({
+        id: parseInt(item.CD_DISCIP),
+        name: item.DS_DISCIP,
+        finalGrade:
+          item.notaFinalCalcField === "-"
+            ? "-"
+            : parseFloat(item.notaFinalCalcField),
+        academicYear: item.CD_LECTIVO,
+        studyYear: parseInt(item.CD_A_S_CUR),
+        semester: parseInt(item.CD_DURACAO.replace("S", "")),
+        ects: parseInt(item.ectsCalcField),
+      })
+    );
+
+    res.status(response.status).json(formattedUnits);
   } catch (error) {
     if (axios.isAxiosError(error)) {
       res
         .status(error.response ? error.response.status : 500)
         .send(error.message);
     } else {
+      console.error(error);
       res.status(500).send("An unexpected error occurred");
     }
   }
