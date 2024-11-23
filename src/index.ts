73f6e863f4b65b98157c090d8272c6004fe4a543
Matthew Rodrigues <matt@capaz.dev>
2024-11-23 17:56:36 +0000

    refactor(auth): implemented router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
-import axios from "axios";
-
+import authRouter from "./auth";
 import academicosRouter from "./routes/academicos/";
 
 const app = express();
@@ -13,61 +12,7 @@ app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-app.post("/login", async (req, res) => {
-  if (!req.body.username || !req.body.password) {
-    res.status(400).send("Missing username or password");
-    return;
-  }
-
-  const { username, password } = req.body;
-
-  try {
-    const response = await axios.post(
-      "https://academicos.ipvc.pt/netpa/ajax?stage=loginstage",
-      {
-        _formsubmitstage: "loginstage",
-        _formsubmitname: "login",
-        ajax_mode: true,
-        _user: username,
-        _pass: password,
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        },
-        withCredentials: true,
-      }
-    );
-
-    const cookies = response.headers["set-cookie"];
-    const token =
-      cookies?.find((cookie) => cookie.includes("JSESSIONID"))?.split(";")[0] ||
-      null;
-
-    res.status(response.status).json({
-      academicos: token,
-    });
-
-    // activate the session
-    axios.get(
-      "https://academicos.ipvc.pt/netpa/page?stage=ConsultaNotasAluno",
-      {
-        headers: {
-          Cookie: token,
-        },
-      }
-    );
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      res
-        .status(error.response ? error.response.status : 500)
-        .send(error.message);
-    } else {
-      res.status(500).send("An unexpected error occurred");
-    }
-  }
-});
-
+app.use("/login", authRouter);
 app.use("/academicos", academicosRouter);
 
 app.listen(port, () => {
