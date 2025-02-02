e1afc4e358bc11c019447122bceb7ad69541b58c
Matthew Rodrigues <mattcapazz@gmail.com>
2025-02-02 14:21:04 +0000

    refactor(auth): renamed main file to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import authRouter from "./auth/auth";
+import authRouter from "./auth";
 import academicosRouter from "./routes/academicos/";
 
 const app = express();
