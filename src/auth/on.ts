e10dd1eb694eff5123112ddee18af507921d1ede
Matthew Rodrigues <mattcapazz@gmail.com>
2025-02-02 15:41:31 +0000

    fix(auth): add content-type header & support multiple cookies in ONStrategy

--- src/auth/on.ts
+++ src/auth/on.ts
@@ -2,18 +2,25 @@ import axios from "axios";
 
 export const ONStrategy = async (username: string, password: string) => {
   try {
-    const response = await axios.post("https://on.ipvc.pt/login.php", {
-      "on-user": username,
-      "on-pass": password,
-      "on-auth": 3,
-    });
+    const response = await axios.post(
+      "https://on.ipvc.pt/login.php",
+      {
+        "on-user": username,
+        "on-pass": password,
+        "on-auth": 3,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+      }
+    );
 
     const cookies = response.headers["set-cookie"];
-    const token =
-      cookies?.find((cookie) => cookie.includes("PHPSESSID"))?.split(";")[0] ||
-      null;
+    const cookie = (name: string) =>
+      cookies?.find((cookie) => cookie.includes(name))?.split(";")[0] || null;
 
-    return token;
+    return `${cookie("ONIPVC")}; ${cookie("PHPSESSID")}`;
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
