2c7ba651b4608e7eb0a90bcbfc4a90208efe23ce
Matthew Rodrigues <mattcapazz@gmail.com>
2025-02-26 20:49:15 +0000

    feat(curricularUnits): restructure units, add avg. grade calculation

--- src/routes/academicos/modules/curricularUnits.ts
+++ src/routes/academicos/modules/curricularUnits.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 import axios from "axios";
 
-interface FormattedUnit {
+interface CurricularUnit {
   id: number;
-  courseId: number;
   name: string;
   year: number;
   semester: number;
   evaluationType: string;
   grade: [string, string, string, string][];
+  highestGrade: string;
   ects: number;
+}
+
+interface Course {
+  avgGrade: number;
+  curricularUnits: CurricularUnit[];
 }
 
 const router = Router();
@@ -36,22 +41,41 @@ router.get("/", async (req, res) => {
       return;
     }
 
-    const unitsMap: { [key: number]: FormattedUnit } = {};
+    const unitsMap: { [key: number]: Course } = {};
 
     response.data.result.forEach((item: any) => {
-      const id = item.CD_DISCIP;
-      if (!unitsMap[id])
-        unitsMap[id] = {
-          id,
-          courseId: item.CD_CURSO,
+      const courseId = item.CD_CURSO,
+        unitId = item.CD_DISCIP;
+
+      if (!unitsMap[courseId]) {
+        unitsMap[courseId] = {
+          avgGrade: 0,
+          curricularUnits: [],
+        };
+      }
+
+      const units = unitsMap[courseId].curricularUnits;
+      let unit = units.find((u) => u.id === unitId);
+
+      if (!unit) {
+        unit = {
+          id: unitId,
           name: item.DS_DISCIP,
           year: item.CD_A_S_CUR,
           semester: parseInt(item.CD_DURACAO_PD.replace("S", "")),
           evaluationType: item.DS_AVALIA,
           grade: [],
+          highestGrade: item.NR_NOT_DIS,
           ects: item.NR_CRE_EUR_PD,
         };
-      unitsMap[id].grade.push([
+
+        units.push(unit);
+      }
+
+      if (unit.highestGrade < item.NR_NOT_DIS)
+        unit.highestGrade = item.NR_NOT_DIS;
+
+      unit.grade.push([
         item.NR_NOT_DIS,
         item.DS_STATUS,
         item.DT_FIM_DIS,
@@ -59,24 +83,31 @@ router.get("/", async (req, res) => {
       ]);
     });
 
-    const formattedUnits = Object.values(unitsMap)
-      .sort((a, b) => {
-        if (a.year < b.year) return -1;
-        if (a.year > b.year) return 1;
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
-      })
-      .map((unit) => {
-        unit.grade.sort((a, b) => {
-          if (a[2] < b[2]) return 1;
-          if (a[2] > b[2]) return -1;
-          return 0;
-        });
-        return unit;
+    const formattedUnits = Object.values(unitsMap).map((course) => {
+      course.avgGrade = parseFloat(
+        course.curricularUnits
+          .reduce(
+            (acc, unit) =>
+              acc +
+              parseFloat(unit.highestGrade) / course.curricularUnits.length,
+            0
+          )
+          .toFixed(2)
+      );
+
+      course.curricularUnits.sort((a, b) => {
+        if (a.year !== b.year) return a.year - b.year;
+        return a.name.localeCompare(b.name);
       });
 
-    res.status(response.status).json(formattedUnits);
+      course.curricularUnits.forEach((unit) => {
+        unit.grade.sort((a, b) => (a[2] > b[2] ? -1 : a[2] < b[2] ? 1 : 0));
+      });
+
+      return course;
+    });
+
+    res.status(200).json(formattedUnits);
   } catch (error) {
     if (axios.isAxiosError(error)) {
       res
