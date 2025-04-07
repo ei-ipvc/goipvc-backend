c56b6d9803a81ca631c307c6b0168515cb217186
Matthew Rodrigues <mattcapazz@gmail.com>
2025-04-07 10:38:09 +0100

    refactor(schedule)!: use GET, query params & lint'd

--- src/routes/on/modules/schedule.ts
+++ src/routes/on/modules/schedule.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import JSON5 from "json5";
 import { Lesson } from "../../../models/lesson";
-import {CheerioAPI} from "cheerio"
+import { CheerioAPI } from "cheerio";
 
 const router = Router();
 
@@ -17,19 +17,18 @@ function parseSchedule($: CheerioAPI) {
     const courseId = parseInt(lesson.datauc.match(/^(\d+)/)[1]);
 
     let [, shortName = "", classType = "", room = ""] =
-        /(.+) \[(.+)] .+- (.+)/.exec(lesson.title)!;
+      /(.+) \[(.+)] .+- (.+)/.exec(lesson.title)!;
 
     const cNameMatch =
-        lesson.datauc.match(/\| (.*) \|/) || lesson.datauc.match(/-(.*?)-/);
+      lesson.datauc.match(/\| (.*) \|/) || lesson.datauc.match(/-(.*?)-/);
     const className = cNameMatch ? cNameMatch[1] : "Desconhecido";
 
     const teachers = lesson.datadocentes
-        .match(/&bull; (.*)<\/div>/)[1]
-        .split("; ")
-        .filter(
-            (t: string, _: number, arr: string[]) =>
-                t !== "N/D" || arr.length === 1
-        );
+      .match(/&bull; (.*)<\/div>/)[1]
+      .split("; ")
+      .filter(
+        (t: string, _: number, arr: string[]) => t !== "N/D" || arr.length === 1
+      );
 
     room = room.replace(/\./, "");
 
@@ -48,14 +47,14 @@ function parseSchedule($: CheerioAPI) {
   });
 }
 
-router.post("/", checkOnAuth, async (req, res) => {
+router.get("/", checkOnAuth, async (req, res) => {
   const token = req.headers["x-auth-on"];
   if (!token) {
     res.status(400).send("Missing token");
     return;
   }
 
-  const studentId = req.body.studentId;
+  const studentId = req.query.studentId as string;
   if (!studentId) {
     res.status(400).send("Missing studentId");
     return;
@@ -81,7 +80,7 @@ router.post("/", checkOnAuth, async (req, res) => {
       );
 
       const $ = cheerio.load(response.data);
-      return parseSchedule($)
+      return parseSchedule($);
     };
 
     const [s1, s2] = await Promise.allSettled([
@@ -108,179 +107,186 @@ router.post("/", checkOnAuth, async (req, res) => {
 });
 
 router.get("/search-options", checkOnAuth, async (req, res) => {
-  const token = req.headers["x-auth-on"]
+  const token = req.headers["x-auth-on"];
 
   const response = await axios.get(
-      "https://on.ipvc.pt/v1/modulos/atividadeletiva/horarios_consulta.php",
-      {
-        headers: {
-          Cookie: token,
-        }
-      }
-  )
+    "https://on.ipvc.pt/v1/modulos/atividadeletiva/horarios_consulta.php",
+    {
+      headers: {
+        Cookie: token,
+      },
+    }
+  );
 
-  const $ = cheerio.load(response.data)
+  const $ = cheerio.load(response.data);
 
   const parseOptions = (selector: string, filterZero = false) => {
     return $(selector)
-        .find('option')
-        .filter((_, el) => !filterZero || $(el).attr('value') !== '0')
-        .map((_, el) => ({
-          name: $(el).text(),
-          value: $(el).attr('value') || ''
-        }))
-        .get()
-  }
+      .find("option")
+      .filter((_, el) => !filterZero || $(el).attr("value") !== "0")
+      .map((_, el) => ({
+        name: $(el).text(),
+        value: $(el).attr("value") || "",
+      }))
+      .get();
+  };
 
   let a = {
-    years: parseOptions('#param_anoletivoH'),
-    semesters: parseOptions('#param_semestreH'),
-    schools: parseOptions('#param_uoH', true),
-    degrees: parseOptions('#param_grauH', true)
-  }
+    years: parseOptions("#param_anoletivoH"),
+    semesters: parseOptions("#param_semestreH"),
+    schools: parseOptions("#param_uoH", true),
+    degrees: parseOptions("#param_grauH", true),
+  };
 
-  res.send(a)
-})
+  res.send(a);
+});
 
 // Usage example (Query params)
 // ?year=202425&degree=LICENCIATURA&school=04
 router.get("/course-list", checkOnAuth, async (req, res) => {
-  const token = req.headers["x-auth-on"]
+  const token = req.headers["x-auth-on"];
 
-  if(!req.query["year"] || !req.query["degree"] || !req.query["school"]) {
-    res.sendStatus(400)
-    return
+  if (!req.query["year"] || !req.query["degree"] || !req.query["school"]) {
+    res.sendStatus(400);
+    return;
   }
 
-  const formData = new FormData()
+  const formData = new FormData();
   // really
-  formData.append("param_anoletivoH", req.query["year"] as string)
-  formData.append("param_grauH", req.query["degree"] as string)
-  formData.append("param_uoH", req.query["school"] as string)
+  formData.append("param_anoletivoH", req.query["year"] as string);
+  formData.append("param_grauH", req.query["degree"] as string);
+  formData.append("param_uoH", req.query["school"] as string);
 
   const response = await axios.post(
-      "https://on.ipvc.pt/v1/modulos/atividadeletiva/source_select_cursosH.php",
-      formData,
-      {
-        headers: {
-          Cookie: token,
-        }
-      }
-  )
+    "https://on.ipvc.pt/v1/modulos/atividadeletiva/source_select_cursosH.php",
+    formData,
+    {
+      headers: {
+        Cookie: token,
+      },
+    }
+  );
 
-  const $ = cheerio.load(response.data)
+  const $ = cheerio.load(response.data);
 
-  res.send($('#param_cursoH')
-      .find('option')
-      .filter((_, el) => $(el).attr('value') !== '0')
+  res.send(
+    $("#param_cursoH")
+      .find("option")
+      .filter((_, el) => $(el).attr("value") !== "0")
       .map((_, el) => ({
         name: $(el).text(),
-        value: $(el).attr('value') || ''
+        value: $(el).attr("value") || "",
       }))
-      .get())
-})
+      .get()
+  );
+});
 
 async function getWeekList(token: string, year: string, semester: string) {
-  const formData = new FormData()
-  formData.append("param_anoletivoH", year)
-  formData.append("param_semestreH", semester)
+  const formData = new FormData();
+  formData.append("param_anoletivoH", year);
+  formData.append("param_semestreH", semester);
 
   const response = await axios.post(
-      "https://on.ipvc.pt/v1/modulos/atividadeletiva/source_select_semanasH.php",
-      formData,
-      {
-        headers: {
-          Cookie: token,
-        }
-      }
-  )
+    "https://on.ipvc.pt/v1/modulos/atividadeletiva/source_select_semanasH.php",
+    formData,
+    {
+      headers: {
+        Cookie: token,
+      },
+    }
+  );
 
-  const $ = cheerio.load(response.data)
+  const $ = cheerio.load(response.data);
 
-  return $('#param_semanaH')
-      .find('option')
-      .map((_, el) => $(el).attr('value'))
-      .get()
+  return $("#param_semanaH")
+    .find("option")
+    .map((_, el) => $(el).attr("value"))
+    .get();
 }
 
 // Usage example (Query params)
 // ?year=202425&semester=S2&course=9119
 router.get("/class-list", checkOnAuth, async (req, res) => {
-  const token = req.headers["x-auth-on"]
+  const token = req.headers["x-auth-on"];
 
-  if(!req.query["year"] || !req.query["semester"] || !req.query["course"]) {
-    res.sendStatus(400)
-    return
+  if (!req.query["year"] || !req.query["semester"] || !req.query["course"]) {
+    res.sendStatus(400);
+    return;
   }
 
-  const formData = new FormData()
-  formData.append("param_anoletivoH", req.query["year"] as string)
-  formData.append("param_semestreH", req.query["semester"] as string)
-  formData.append("param_cursoH", req.query["course"] as string)
+  const formData = new FormData();
+  formData.append("param_anoletivoH", req.query["year"] as string);
+  formData.append("param_semestreH", req.query["semester"] as string);
+  formData.append("param_cursoH", req.query["course"] as string);
 
   const response = await axios.post(
-      "https://on.ipvc.pt/v1/modulos/atividadeletiva/source_select_turmasH.php",
-      formData,
-      {
-        headers: {
-          Cookie: token,
-        }
-      }
-  )
+    "https://on.ipvc.pt/v1/modulos/atividadeletiva/source_select_turmasH.php",
+    formData,
+    {
+      headers: {
+        Cookie: token,
+      },
+    }
+  );
 
-  const $ = cheerio.load(response.data)
+  const $ = cheerio.load(response.data);
 
-  res.send($('#param_turmaH')
-      .find('option')
-      .filter((_, el) => $(el).attr('value') !== '0')
+  res.send(
+    $("#param_turmaH")
+      .find("option")
+      .filter((_, el) => $(el).attr("value") !== "0")
       .map((_, el) => ({
         name: $(el).text(),
-        value: $(el).attr('value') || ''
+        value: $(el).attr("value") || "",
       }))
-      .get())
-})
+      .get()
+  );
+});
 
 // Usage example (Query params)
 // ?year=202425&semester=S2&classId=EI-3-A
 router.get("/search", checkOnAuth, async (req, res) => {
-  const token = req.headers["x-auth-on"]
+  const token = req.headers["x-auth-on"];
 
-  if(!req.query["year"] || !req.query["semester"] || !req.query["classId"]) {
-    res.sendStatus(400)
-    return
+  if (!req.query["year"] || !req.query["semester"] || !req.query["classId"]) {
+    res.sendStatus(400);
+    return;
   }
 
-  const weeks = await getWeekList(token as string, req.query["year"] as string, req.query["semester"] as string)
+  const weeks = await getWeekList(
+    token as string,
+    req.query["year"] as string,
+    req.query["semester"] as string
+  );
 
-  let schedule: any[] = []
+  let schedule: any[] = [];
 
-  for(const week of weeks) {
-    const formData = new FormData()
-    formData.append("param_anoletivoH", req.query["year"] as string)
-    formData.append("param_semestreH", req.query["semester"] as string)
-    formData.append("param_turmaH", req.query["classId"] as string)
-    formData.append("param_semanaH", week)
-    formData.append("emissorH", "consultageral")
+  for (const week of weeks) {
+    const formData = new FormData();
+    formData.append("param_anoletivoH", req.query["year"] as string);
+    formData.append("param_semestreH", req.query["semester"] as string);
+    formData.append("param_turmaH", req.query["classId"] as string);
+    formData.append("param_semanaH", week);
+    formData.append("emissorH", "consultageral");
 
     const response = await axios.post(
-        "https://on.ipvc.pt/v1/modulos/atividadeletiva/horario_source_v3.php",
-        formData,
-        {
-          headers: {
-            Cookie: token,
-          }
-        }
-    )
+      "https://on.ipvc.pt/v1/modulos/atividadeletiva/horario_source_v3.php",
+      formData,
+      {
+        headers: {
+          Cookie: token,
+        },
+      }
+    );
 
-    const $ = cheerio.load(response.data)
+    const $ = cheerio.load(response.data);
 
-    if($.text().includes("Sem horário definido"))
-      continue
+    if ($.text().includes("Sem horário definido")) continue;
 
-    schedule = schedule.concat(parseSchedule($))
+    schedule = schedule.concat(parseSchedule($));
   }
 
-  res.send(schedule)
-})
+  res.send(schedule);
+});
 
 export default router;
